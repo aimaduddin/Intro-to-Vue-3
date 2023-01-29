dbb5f5abb820b02fa1fb72dada937fc2fadb03aa
aimaduddin <aima.aimaduddin@gmail.com>
2023-01-29 21:47:26 +0800

    L2 End

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-const product = 'Socks'
+const app = Vue.createApp({
+    data(){
+        return{
+            product: 'Lamp',
+            description: 'This lamp is made by Xiaomi'
+        }
+    }
+})
